240208194f0cf3c174f7b8466f66c30ad616ccf0
Brian Doherty <bdoherty@constantcontact.com>
2016-03-14 22:17:54 -0400

    Moving mocks to seperate fixture file for @blamattina

--- spec/pc-spec.js
+++ spec/pc-spec.js
@@ -1,35 +1,9 @@
 var rewire = require('rewire');
 var PC = rewire('../lib/pc');
 var Promise = require('bluebird');
+var mockFiles = require('./support/fixtures/mock-files');
 
 describe('Paper Cassette', function () {
-  var mockFiles = {
-    'some/directory/cactus.xml': {
-      contents: '<xml>Cactus</xml>',
-    },
-    'some/diva/falcon.xml': {
-      contents: '<xml>Falcon</xml>',
-    },
-    'some/diva/cbl.xml': {
-      contents: '<xml>CBL</xml>',
-    },
-    'some/buzz.xml': {
-      contents: '<xml>buzz</xml>',
-    },
-    'some/diva/directory/blamaste.xml': {
-      contents: '<xml>Blamaste</xml>',
-    },
-    'some/diva/president/oblama.xml': {
-      contents: '<xml>oblama</xml>',
-    },
-    'some/bad-blammajamma.xml': {
-      contents: '<xml>bad-blamma-jamma</xml>',
-    },
-    'some/guy/named/Gondor.xml': {
-      contents: '<xml>Gondor</xml>',
-    },
-  };
-
   var mockFs = {
     writeFileAsync: jasmine.createSpy('writeFileAsync').and.callFake(function () {
       return true;
